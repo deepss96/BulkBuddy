7d0dd5cd127e3374281c634c1fa913f1e38e5f4b
Deepak Vaishnav <111363738+deepss96@users.noreply.github.com>
2026-09-14 12:26:42 +0530

    Add healthcheck route to microservice

--- whatsapp-microservice/index.js
+++ whatsapp-microservice/index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.get('/', (req, res) => res.send('WhatsApp Microservice is running!'));
+
 const clients = {};
 const qrCodes = {};
 const statuses = {}; // 'connecting', 'connected', 'disconnected', 'failed'
